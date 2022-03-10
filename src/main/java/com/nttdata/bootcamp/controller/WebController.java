a19e9372083353aa80a3b1c97cc6e7710a043cfb
DanielRuizMed <drm19933@gmail.com>
2022-03-10 10:12:36 +0100

    comit inicial

---
+++ src/main/java/com/nttdata/bootcamp/controller/WebController.java
@@ -0,0 +1,25 @@
+package com.nttdata.bootcamp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WebController {
+
+	@GetMapping("/saludo")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="Mundo") 
+							String nombre, Model model) {
+		model.addAttribute("name", nombre);
+		return "hola";
+	}
+	
+	@GetMapping("/error")
+	public String error_page(){
+		return "error";
+	}
+	
+	
+}
+
